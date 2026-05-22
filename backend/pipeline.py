26b9f625b35f3dd87477efa927bbdac839451f40
Raunak Kumar Jha <raunak3208@users.noreply.github.com>
2026-05-22 20:21:35 +0530

    fix

--- backend/pipeline.py
+++ backend/pipeline.py
@@ -1,7 +1,7 @@
 from agents import build_reader_agent , build_search_agent , writer_chain , critic_chain
 
 def run_research_pipeline(topic : str) -> dict:
-
+    
     state = {}
 
     #search agent working 
